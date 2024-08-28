63ac599a1b93e9449d1797924b0e2d5123c5a9b1
KoreaMoney <kimdowon1404@gmail.com>
2024-08-28 21:51:18 +0900

    test4

--- middleware.js
+++ middleware.js
@@ -4,14 +4,12 @@ import { checkAuth } from "@/lib/checkAuth";
 // 인증이 필요한 경로들을 배열로 정의
 const protectedRoutes = ["/projects"];
 
-export async function middleware(req) {
+export default async function middleware(req) {
   const { pathname } = req.nextUrl;
-  console.log("pathname", pathname);
-  console.log("req", req);
+
   // 보호된 경로인지 확인
   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
     const authResult = await checkAuth(req);
-    console.log("authResult", authResult);
     if (!authResult.authenticated) {
       return NextResponse.redirect(new URL(authResult.redirectUrl, req.url));
     }
@@ -23,5 +21,5 @@ export async function middleware(req) {
 
 // 미들웨어를 적용할 경로 설정
 export const config = {
-  matcher: ["/projects/:path*"],
+  matcher: ["/projects/:path*", "/api/:path*", "/((?!_next/static|favicon.ico).*)"],
 };
