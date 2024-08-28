1c23f882f9a9abd86b75ef6be5226be6fe699218
KoreaMoney <kimdowon1404@gmail.com>
2024-08-28 21:58:04 +0900

    test6

--- middleware.js
+++ middleware.js
@@ -7,6 +7,14 @@ const protectedRoutes = ["/projects"];
 export default async function middleware(req) {
   const { pathname } = req.nextUrl;
 
+  // 쿠키에서 세션 토큰 확인
+  const sessionToken = req.cookies.get("__Secure-authjs.session-token");
+
+  // 세션 토큰이 있으면 인증된 사용자로 간주하고 다음으로 진행
+  if (sessionToken) {
+    return NextResponse.next();
+  }
+
   // 보호된 경로인지 확인
   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
     const authResult = await checkAuth(req);
@@ -14,11 +22,6 @@ export default async function middleware(req) {
       return NextResponse.redirect(new URL(authResult.redirectUrl, req.url));
     }
   }
-  console.log("pathname", pathname);
-  console.log(
-    "pathtestname",
-    protectedRoutes.some((route) => pathname.startsWith(route))
-  );
 
   // 인증이 필요없는 경로는 그대로 진행
   return NextResponse.next();
